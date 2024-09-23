2e2f685f4291b18f6af177a99c11e0d194581cc1
MariaDimian <mariad134@nycstudents.net>
2024-09-23 09:20:49 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,23 @@
-number = [1,2,3,4,5,6]
-print(number[3])
-#number
-color = input("what color is the sky?")
-if color == "blue":
-    print("correct")
-else:
-    print("incorrect")
-#color
+# number = [1,2,3,4,5,6]
+# print(number[3])
+# #number
+# color = input("what color is the sky?")
+# if color == "blue":
+#     print("correct")
+# else:
+#     print("incorrect")
+# #color
 
-def evenodd()
-evenodd=int(input("give me a number: ")):
-if evenodd % 2 == 0:
-    print("even")
-else:
-    print("odd")
+# def evenodd():
+#     evenodd=int(input("give me a number: "))
+#     if evenodd % 2 == 0:
+#         print("even")
+#     else:
+#         print("odd")
+
+# evenodd()
+
+bill = int(input("whats the bill"))
+
+def thing(bill):
+    print(int(bill))

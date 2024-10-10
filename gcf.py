70a9ba31fee22e329788922d2d6619b59777363b
MariaDimian <mariad134@nycstudents.net>
2024-10-10 09:06:49 -0400

    Update gcf.py

--- gcf.py
+++ gcf.py
@@ -2,7 +2,7 @@ num_1 = int(input("give me a number"))
 num_2 = int(input("give me  another number"))
 gcf = 1
 
-for i in range(1, min(num_1, num_2))
-    if 
-
-print gcf
+for i in range(1, min(num_1, num_2)):
+    if num_1 % i == 0 and num_2 % i == 0:
+        gcf = i
+print(f"{gcf}")
